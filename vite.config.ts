68e65df0d49bf23c13db4226c004230258d93294
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2026-02-27 21:06:32 -0500

    added version in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/Feedback-Form/",
+  define: {
+    'process.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version)
+  }
 });
